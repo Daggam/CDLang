217e565b80f282996fd56f5b93602905c9212911
Benjamin Villa <daggamarg@gmail.com>
2026-07-06 21:55:15 -0300

    fix: cambiar nombre de la carpeta de "repl" a "cdlang"

---
+++ cmd/cdlang/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Printf("Bienvenido a CDL v1.0.0.\n")
+	Start(os.Stdin, os.Stdout)
+
+}
---
+++ cmd/cdlang/repl.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/Daggam/CDL/internal/evaluator"
+	"github.com/Daggam/CDL/internal/explain"
+	"github.com/Daggam/CDL/internal/lexer"
+	"github.com/Daggam/CDL/internal/object"
+	"github.com/Daggam/CDL/internal/parser"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	reader := bufio.NewReader(in)
+	env := object.NewEnvironment()
+
+	for {
+		fmt.Printf(PROMPT)
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			panic("Hubo un error no esperado")
+		}
+
+		if explain.IsExplain(line) {
+			fmt.Fprint(out, explain.Build(line))
+			continue
+		}
+
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
+		}
+		evaluated := evaluator.Eval(program, env)
+		if errObj, ok := evaluated.(*object.Error); ok {
+			io.WriteString(out, "[EVALUATOR] "+errObj.Message)
+			io.WriteString(out, "\n")
+			continue
+		}
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+		//io.WriteString(out, program.String())
+		//io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "[PARSER] "+msg+"\n")
+	}
+}
